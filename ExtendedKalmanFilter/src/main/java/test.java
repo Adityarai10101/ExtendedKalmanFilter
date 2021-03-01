5af068b3f9205982e7ffcc5c6c84b078fe21828e
Jeffrey Qian <jeffreyqdd@gmail.com>
2021-03-01 17:48:59 -0600

    branch test

--- ExtendedKalmanFilter/src/main/java/test.java
+++ ExtendedKalmanFilter/src/main/java/test.java
@@ -1,7 +1,13 @@
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 public class test {
 
-#commit by jeff
+    // new commit by jeff
+    Vector2D test = new Vector2D(1,2);
+
+    test(){
+
+    }
 
 }
